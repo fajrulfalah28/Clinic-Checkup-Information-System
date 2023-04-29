c79f79880d1fef26134804da7f57f74f628a160b
Tuupi <fajiom@gmail.com>
2023-04-30 02:19:45 +0700

    adding libraries

--- PatientManagement/src/mainApp/App.java
+++ PatientManagement/src/mainApp/App.java
@@ -1,7 +1,10 @@
 package mainApp;
 
+import java.sql.Driver;
+
 public class App {
 	public static void main(String[] args) {
+		// Driver server = new com.microsof
 		LoginMenu loginMenu = new LoginMenu();
 	}
 }
